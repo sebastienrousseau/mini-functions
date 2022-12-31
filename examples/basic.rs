65768c7d252fb90a7ca65b9b17020af1c01d585a
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-12-31 09:13:53 +0000

    feat(workflows): minor updates

---
+++ examples/basic.rs
@@ -0,0 +1,32 @@
+
+use mini_functions::date::Date;
+
+fn main() {
+    let date = Date::date();
+    let day = Date::day();
+    let hour = Date::hour();
+    let microsecond = Date::microsecond();
+    let millisecond = Date::millisecond();
+    let minute = Date::minute();
+    let month = Date::month();
+    let nanosecond = Date::nanosecond();
+    let now_utc = Date::now_utc();
+    let second = Date::second();
+    let timestamp = Date::timestamp();
+    let weekday = Date::weekday();
+    let year = Date::year();
+
+    println!("✅ Date::date():          {}", date);
+    println!("✅ Date::day():           {}", day);
+    println!("✅ Date::hour():          {}", hour);
+    println!("✅ Date::microsecond():   {}", microsecond);
+    println!("✅ Date::millisecond():   {}", millisecond);
+    println!("✅ Date::minute():        {}", minute);
+    println!("✅ Date::month():         {}", month);
+    println!("✅ Date::nanosecond():    {}", nanosecond);
+    println!("✅ Date::now():           {}", now_utc);
+    println!("✅ Date::second():        {}", second);
+    println!("✅ Date::timestamp():     {}", timestamp);
+    println!("✅ Date::weekday():       {}", weekday);
+    println!("✅ Date::year():          {}", year);
+}
