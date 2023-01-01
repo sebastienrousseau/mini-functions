c9516621e713eb2258dde6b64a7b2d6208d15a28
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-01-01 20:21:40 +0000

    fix(tests) : Updates and comments

---
+++ tests/log.rs
@@ -0,0 +1,71 @@
+#[cfg(test)]
+mod tests {
+    use mini_functions::{date::Date, log::Log};
+
+    #[test]
+    fn test_log() {
+        let log = Log::new(
+            "12345678-1234-1234-1234-1234567890ab",
+            &Date::now_utc(),
+            "INFO",
+            "SystemTrayEvent",
+            "Showing main window",
+        );
+        log.log();
+    }
+    #[test]
+    fn test_log_error() {
+        let log = Log::new(
+            "12345678-1234-1234-1234-1234567890ab",
+            &Date::now_utc(),
+            "INFO",
+            "SystemTrayEvent",
+            "Showing main window",
+        );
+        log.log();
+    }
+    #[test]
+    fn test_log_warn() {
+        let log = Log::new(
+            "12345678-1234-1234-1234-1234567890ab",
+            &Date::now_utc(),
+            "INFO",
+            "SystemTrayEvent",
+            "Showing main window",
+        );
+        log.log();
+    }
+    #[test]
+    fn test_log_debug() {
+        let log = Log::new(
+            "12345678-1234-1234-1234-1234567890ab",
+            &Date::now_utc(),
+            "INFO",
+            "SystemTrayEvent",
+            "Showing main window",
+        );
+        log.log();
+    }
+    #[test]
+    fn test_log_trace() {
+        let log = Log::new(
+            "12345678-1234-1234-1234-1234567890ab",
+            &Date::now_utc(),
+            "INFO",
+            "SystemTrayEvent",
+            "Showing main window",
+        );
+        log.log();
+    }
+    #[test]
+    fn test_log_info() {
+        let log = Log::new(
+            "12345678-1234-1234-1234-1234567890ab",
+            &Date::now_utc(),
+            "INFO",
+            "SystemTrayEvent",
+            "Showing main window",
+        );
+        log.log();
+    }
+}
