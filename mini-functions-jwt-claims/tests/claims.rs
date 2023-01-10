ae7cb3471afc57dafc03ff9766f0687426be0290
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-01-10 23:02:18 +0000

    fix(claims): added missing fields

--- mini-functions-jwt-claims/tests/claims.rs
+++ mini-functions-jwt-claims/tests/claims.rs
@@ -11,21 +11,30 @@ mod tests {
     #[test]
     fn test_claims() {
         let claims = {
-            let iss: &str = &"mini_functions_claims".to_string();
-            let sub: &str = &"John Doe".to_string();
             let aud: &str = &"1516239022";
+            let custom: &str = &"John Doe".to_string();
+            let did: &str = &"mini_functions_claims".to_string();
             let exp: &str = &"1516239022";
-            let nbf: &str = &"John Doe".to_string();
             let iat: &str = &"John Doe".to_string();
+            let iss: &str = &"mini_functions_claims".to_string();
             let jti: &str = &"John Doe".to_string();
+            let nbf: &str = &"John Doe".to_string();
+            let sub: &str = &"John Doe".to_string();
+            let vc: &str = &"John Doe".to_string();
+            let vp: &str = &"John Doe".to_string();
+
             Claims {
-                iss: Some(iss.to_string()),
-                sub: Some(sub.to_string()),
                 aud: Some(aud.to_string()),
+                custom: Some(custom.to_string()),
+                did: Some(did.to_string()),
                 exp: Some(exp.to_string()),
-                nbf: Some(nbf.to_string()),
                 iat: Some(iat.to_string()),
+                iss: Some(iss.to_string()),
                 jti: Some(jti.to_string()),
+                nbf: Some(nbf.to_string()),
+                sub: Some(sub.to_string()),
+                vc: Some(vc.to_string()),
+                vp: Some(vp.to_string()),
             }
         };
         println!("🦀 Claims::new():                 ✅ {:?}\n", claims);
