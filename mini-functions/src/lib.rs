4a1f7c507a95438e6e58b833e88e47937bf8e2ab
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-01-11 11:25:46 +0000

    test(release): re-testing release workflow

---
+++ mini-functions/src/lib.rs
@@ -0,0 +1,6 @@
+//! A wrapper around the `mini-functions` crates.
+//!
+//! This crate is a wrapper around the `mini-functions` crate. It is
+//! used to provide a more convenient API for the `mini-functions`
+//! crate.
+//!
