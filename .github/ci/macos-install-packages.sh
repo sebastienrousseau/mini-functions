e78d3745916d0d8280a3c310e0fab4c658db8104
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-01-11 16:16:38 +0000

    fix(ci): new flow

--- .github/ci/macos-install-packages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-brew install openssl@1.1
-brew install asciidoctor
--- .github/ci/ubuntu-install-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends \
-    asciidoctor \
-    pkg-config \
-    openssl1.0 \
-    libssl1.0.0 \
-    libssl1.0-dev \
-    zsh xz-utils liblz4-tool musl-tools
